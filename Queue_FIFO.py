69fb9860ab74deb33991d1368b6fbab96661abe2
DESKTOP-VM4D9GJ <jolyndonjr@gmail.com>
2022-12-17 17:44:00 +0800

    Initial Files for Queues and Stack

---
+++ Queue_FIFO.py
@@ -0,0 +1,19 @@
+from queues import Queue
+
+fifo = Queue()
+fifo.enqueue("1st")
+fifo.enqueue("2nd")
+fifo.enqueue("3rd")
+
+# fifo.dequeue()
+
+# -----------------------
+
+# fifo = Queue("1st", "2nd", "3rd")
+
+# len(fifo)
+
+# for element in fifo:
+#     print(element)
+
+# len(fifo)
