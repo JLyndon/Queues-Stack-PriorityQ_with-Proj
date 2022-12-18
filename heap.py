780528d55440fad208bd838914e99f86bd646d18
DESKTOP-VM4D9GJ <jolyndonjr@gmail.com>
2022-12-18 11:28:39 +0800

    Added Files for heap Demo

---
+++ heap.py
@@ -0,0 +1,17 @@
+# >>> from heapq import heappush
+
+# >>> fruits = []
+# >>> heappush(fruits, "orange")
+# >>> heappush(fruits, "apple")
+# >>> heappush(fruits, "banana")
+
+# >>> fruits
+
+#-------------------------------------
+
+# >>> from heapq import heappop
+
+# >>> heappop(fruits)
+# 'apple'
+
+# >>> fruits
